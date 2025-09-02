d9c28d759308828dd35b0453230819fb477b591a
Bavani Sankar <wakingbavani@gmail.com>
2025-09-02 16:32:30 +0530

    SC1 - refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,11 +3,12 @@
 class StringCalculator
   DELIMITER_REGEX = /,|\n/
 
+  def delimiter(string)
+    return string[2] if string[0..1] == '//'
+    DELIMITER_REGEX
+  end
+
   def add(numbers)
-    if numbers[0..1] == '//'
-      delimiter = numbers[2]
-      return numbers[4..-1].split(delimiter).sum(&:to_i)
-    end
-    numbers.split(DELIMITER_REGEX).sum(&:to_i)
+    numbers.split(delimiter(numbers)).sum(&:to_i)
   end
 end
